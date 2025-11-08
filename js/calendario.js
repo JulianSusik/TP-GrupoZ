4c91b9e20025a54f802307b84619ce91d205ae6c
Julian Susik <juliansusik10@gmail.com>
2025-11-08 16:14:16 -0300

    agrego pop up de cursos y actualizo calendario

---
+++ js/calendario.js
@@ -0,0 +1,13 @@
+const openModalHTML = document.querySelector('.curso__html');
+const openModalJS = document.querySelector('.curso__js');
+const modalHTML = document.getElementById('modal-html');
+const modalJS = document.getElementById('modal-js');
+const closeButtons = document.querySelectorAll('.modal__close');
+openModalHTML.addEventListener('click', (e) => { e.preventDefault(); modalHTML.classList.add('modal--show'); });
+openModalJS.addEventListener('click', (e) => { e.preventDefault(); modalJS.classList.add('modal--show'); });
+closeButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        const modal = button.closest('.modal'); modal.classList.remove('modal--show');
+    });
+});
